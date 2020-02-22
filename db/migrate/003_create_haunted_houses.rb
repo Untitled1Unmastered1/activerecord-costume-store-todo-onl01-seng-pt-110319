941b28dfa60c12a826268aa5be203990fd8d57c3
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-22 21:01:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :location
-      t.
+      t.string :theme
+      
       
       
